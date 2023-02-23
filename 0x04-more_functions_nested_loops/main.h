3e1a67a3c16358967194a2ef6e1e20ef61b5bb28
Muthoniann <annmuthonim7@gmail.com>
2023-02-23 02:56:28 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File - main.h
+ * Author: Muthoni 
+ * Descprition: Header contains all prototypes used in 0x04-more_functions_nested_loops directory
+*/
+int _putchar(char c);
+
+
+#endif
